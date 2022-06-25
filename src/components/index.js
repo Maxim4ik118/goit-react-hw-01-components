a7668d89db35e935fc52486a0969ecb3e5bbd2d2
Maxi <mmaximonchuk@gmail.com>
2022-06-25 23:20:03 +0300

    new: prod

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as TransactionHistory } from "./TransactionHistory/TransactionHistory";
-export { default as Profile } from "./Profile/Profile";
-export { default as Statistics } from "./Statistics/Statistics";
-export { default as FriendsList } from "./FriendsList/FriendsList";
