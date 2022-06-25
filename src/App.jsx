28ecde2a580fadfc22fe2e283f4c3376fd241c56
Maxi <mmaximonchuk@gmail.com>
2022-06-25 23:18:05 +0300

    update: some fix

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { FriendsList, Profile, Statistics, TransactionHistory } from './components';
+
+import transactionsList from './transactions';
+import profileData from './user';
+import statsData from './data';
+import friendsData from './friends';
+
+function App() {
+  return (
+    <div className="App">
+      <TransactionHistory items={transactionsList} />
+      <Profile {...profileData} />
+      <Statistics stats={statsData} />
+      <FriendsList friends={friendsData} />
+    </div>
+  );
+}
+
+export default App;
